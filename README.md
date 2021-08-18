bf96b90affa85e91dfb5323ef65eb826335b977e
Mario Martínez L <mmartinezl@ucenfotec.ac.cr>
2021-08-18 12:35:22 -0600

    Se agregan los logger a CondominioController, y se agrega el texto en el archivo Readme.md.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# componentes-condominio
+# BISOFT-15 Diseño y construcción de componentes 2021-2
+
+## Examen final
+
+### Funcionalidades
+
+* Registro de condominio: nombre, dirección, cedula juritica de la asociación, representante de los condóminos, cantidad de unidades y cuota condominal
+* Listar condominios, activos e inactivos
+* Modificar datos del condominio, registro histórico de la cuota condominal
+* Desactivar un condominio
+* Registrar amenidades de un condominio
+* Dado un condominio (activo o inactivo) listar las amenidades
+* Listar los históricos de la cuota condominal
--- src/main/java/com/cenfotec/condominio/controller/CondominioController.java
+++ src/main/java/com/cenfotec/condominio/controller/CondominioController.java
@@ -2,6 +2,8 @@ package com.cenfotec.condominio.controller;
 
 import com.cenfotec.condominio.domian.Condominio;
 import com.cenfotec.condominio.service.CondominioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,21 +14,26 @@ import java.util.Optional;
 @RestController
 @RequestMapping({"/condominios"})
 public class CondominioController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CondominioService condominioService;
 
     @PostMapping
     public Condominio createCondo(@RequestBody Condominio condominio) {
+        logger.info("Creando un condominio", condominioService.saveCondo(condominio).get());
         return condominioService.saveCondo(condominio).get();
     }
 
     @GetMapping
     public List getAllCondos() {
+        logger.info("generando lista de condominios", condominioService.getAllCondos());
         return condominioService.getAllCondos();
     }
 
     @GetMapping(path = {"/{id}"})
     public ResponseEntity<Condominio> getCondoById(@PathVariable long id) {
+        logger.info("Buscando un condominio por id", condominioService.findCondoById(id));
         Optional<Condominio> result = condominioService.findCondoById(id);
         /*
         if (result.isPresent()) return ResponseEntity.ok().body(result.get());
@@ -38,6 +45,7 @@ public class CondominioController {
 
     @PutMapping
     public ResponseEntity<Condominio> updateCondo(@RequestBody Condominio condominio) {
+        logger.info("Actualizando un condominio", condominioService.updateCondo(condominio));
         Optional<Condominio> result = condominioService.updateCondo(condominio);
         /*
         if (result.isPresent()) return ResponseEntity.ok().body(result.get());
@@ -49,6 +57,7 @@ public class CondominioController {
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<?> deleteCondo(@PathVariable("id") long id) {
+        logger.info("Eliminando un condominio", condominioService.deleteCondo(id));
         if (condominioService.deleteCondo(id)) return ResponseEntity.ok().build();
         return ResponseEntity.notFound().build();
     }
